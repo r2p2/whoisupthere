c341ca6c42274184f7d92d3d7796fb6b0375a0f6
Robert Peters <r2p2.gw@gmail.com>
2018-03-24 12:17:13 +0100

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![allow(unused_imports)]
 
-extern crate serde_json;
-extern crate reqwest;
 extern crate clap;
+extern crate reqwest;
+extern crate serde_json;
 
-use serde_json::{Value, Error};
-use clap::{Arg, App};
+use serde_json::{Error, Value};
+use clap::{App, Arg};
 
 #[derive(PartialEq)]
 struct Human {
@@ -15,14 +15,13 @@ struct Human {
 
 fn from_json(data: &str) -> Vec<Human> {
     let mut humans = Vec::new();
-    
+
     let msg: Value = serde_json::from_str(data).expect("json error");
     for human in msg["people"].as_array().unwrap().iter() {
-        humans.push(
-            Human {
-                name: String::from(human["name"].as_str().unwrap()),
-                ship: String::from(human["craft"].as_str().unwrap()),
-            });
+        humans.push(Human {
+            name: String::from(human["name"].as_str().unwrap()),
+            ship: String::from(human["craft"].as_str().unwrap()),
+        });
     }
 
     humans
@@ -42,17 +41,19 @@ fn main() {
         .version("0.1.0")
         .author("Robert Peters <r2p2.gw@gmail.com>")
         .about("Show who is in space right now")
-        .arg(Arg::with_name("count")
-             .short("c")
-             .long("count")
-             .help("Prints only the number of people"))
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .help("Prints only the number of people"),
+        )
         .get_matches();
 
     if matches.occurrences_of("count") > 0 {
         println!("{}", fetch_who_is_up_there().len());
         return;
     }
-        
+
     for human in fetch_who_is_up_there().iter() {
         println!("{}, {}", human.name, human.ship);
     }
@@ -63,19 +64,47 @@ mod tests {
     use super::*;
 
     fn example_data() -> &'static str {
-        r#"{"message": "success", "number": 6, "people": [{"name": "Anton Shkaplerov", "craft": "ISS"}, {"name": "Scott Tingle", "craft": "ISS"}, {"name": "Norishige Kanai", "craft": "ISS"}, {"name": "Oleg Artemyev", "craft": "Soyuz MS-08"}, {"name": "Andrew Feustel", "craft": "Soyuz MS-08"}, {"name": "Richard Arnold", "craft": "Soyuz MS-08"}]}"#
+        r#"{
+"message": "success",
+"number": 6,
+"people": [
+  {"name": "Anton Shkaplerov", "craft": "ISS"},
+  {"name": "Scott Tingle", "craft": "ISS"},
+  {"name": "Norishige Kanai", "craft": "ISS"},
+  {"name": "Oleg Artemyev", "craft": "Soyuz MS-08"},
+  {"name": "Andrew Feustel", "craft": "Soyuz MS-08"},
+  {"name": "Richard Arnold", "craft": "Soyuz MS-08"}]
+}"#
     }
-    
+
     #[test]
     fn parse_successful_data() {
-        let expected_humans = vec! [
-            Human { name: String::from("Anton Shkaplerov"), ship: String::from("ISS") },
-            Human { name: String::from("Scott Tingle")    , ship: String::from("ISS") },
-            Human { name: String::from("Norishige Kanai") , ship: String::from("ISS") },
-            Human { name: String::from("Oleg Artemyev")   , ship: String::from("Soyuz MS-08") },
-            Human { name: String::from("Andrew Feustel")  , ship: String::from("Soyuz MS-08") },
-            Human { name: String::from("Richard Arnold")  , ship: String::from("Soyuz MS-08") },
-            ];
+        let expected_humans = vec![
+            Human {
+                name: String::from("Anton Shkaplerov"),
+                ship: String::from("ISS"),
+            },
+            Human {
+                name: String::from("Scott Tingle"),
+                ship: String::from("ISS"),
+            },
+            Human {
+                name: String::from("Norishige Kanai"),
+                ship: String::from("ISS"),
+            },
+            Human {
+                name: String::from("Oleg Artemyev"),
+                ship: String::from("Soyuz MS-08"),
+            },
+            Human {
+                name: String::from("Andrew Feustel"),
+                ship: String::from("Soyuz MS-08"),
+            },
+            Human {
+                name: String::from("Richard Arnold"),
+                ship: String::from("Soyuz MS-08"),
+            },
+        ];
 
         let humans = from_json(example_data());
         assert_eq!(6, humans.len());
